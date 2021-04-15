f5db94c38eae329a8ba76557dd7ff9472a7c67e8
PLOheMfW <meg4mag@gmail.com>
2021-04-15 15:55:24 +0300

    create models

---
+++ config.py
@@ -0,0 +1,5 @@
+import pathlib
+BASE_DIR = pathlib.Path(__file__).parent
+class Config():
+    SQLALCHEMY_DATABASE_URL = 'sqlite:///'+str(BASE_DIR/"data"/"db.sqlite3")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- src/routes.py
+++ src/routes.py
@@ -5,4 +5,52 @@ class Smoke(Resource):
     def get(self):
         return {"message":200}, 200
 
+def get_all_films():
+    return [{'id':'1', 'title':'Harry Pot','release':'2000'},{'id':'2', 'title':'Harry Shpot','release':'2050'},{'id':'3', 'title':'Harry Snoop','release':'2051'}]
+
+def get_all_films_by_uuid(uuid:str) -> dict:
+    films = get_all_films()
+    film = list(filter( lambda x: x["id"]==uuid , films))
+    return film[0] if film else {}
+
+def create_film(film_json:dict) -> list[dict]:
+    films = get_all_films()
+    films.append(film_json)
+    return films
+
+
+class FilmListApi(Resource):
+    def get(self, uuid=None):
+        if not uuid:
+            films = get_all_films()
+            return films, 200
+        film = get_all_films_by_uuid(uuid)
+        if not film:
+            return '', 404
+        else:
+            return film, 200
+            
+    def post(self):
+        film_json = request.json
+        return create_film(film_json), 201
+    def put(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+
+class FilmListApi(Resource):
+    def get(self, uuid=None):
+        pass   
+    def post(self):
+        pass
+    def put(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+
 api.add_resource(Smoke, "/smoke", strict_slashes = False)
+api.add_resource(FilmListApi, "/films", "/films/<uuid>", strict_slashes = False)
